e9397c882a5bf493e7e44626be587efdedcb043e
nklmish <coding.aurora@gmail.com>
2018-06-20 16:25:52 +0200

    polish

--- src/main/kotlin/com/nklmish/demo/AxonCasinoApplication.kt
+++ src/main/kotlin/com/nklmish/demo/AxonCasinoApplication.kt
@@ -11,14 +11,11 @@ import org.axonframework.eventhandling.scheduling.EventScheduler
 import org.axonframework.eventhandling.scheduling.java.SimpleEventScheduler
 import org.axonframework.serialization.json.JacksonSerializer
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Profile
 import org.springframework.scheduling.annotation.EnableScheduling
-import java.util.*
 import java.util.concurrent.Executors
 
 
